ee6bbcbb11e4ca4baf72bfa6c2ca5a44e4f97b91
Вика <ruseckaya@cmit.ru>
2023-10-29 21:15:16 +0300

    start webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { ANSWER } from "./module";
+import "./index.html";
+import "./style.scss";
+
+console.log("Universe answer: ", ANSWER);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV === "development";
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"}),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+    },
+}
